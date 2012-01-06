e883a1f97f2a8beb9901cc0d372091f62ccb53ed
Reilly Watson <reillywatson@gmail.com>
2012-01-05 22:11:05 -0500

    Admin support for model

---
+++ rolodx/main/admin.py
@@ -0,0 +1,9 @@
+from main.models import Pro, User, Category, ProCategories, Rating, ProRatings
+from django.contrib import admin
+
+admin.site.register(Pro)
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(ProCategories)
+admin.site.register(Rating)
+admin.site.register(ProRatings)
