0ab3c151ea3b53366281af6055c6a9e445beff07
Reilly Watson <reillywatson@gmail.com>
2012-01-05 22:28:21 -0500

    Remove some junk

--- rolodx/urls.py
+++ rolodx/urls.py
@@ -5,14 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'main.views.home'),
-    # url(r'^$', 'rolodx.views.home', name='home'),
-    # url(r'^rolodx/', include('rolodx.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
