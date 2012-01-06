f1739ecc33192f5e3215f56f46fc4e8c565d1e2f
Reilly Watson <reillywatson@gmail.com>
2012-01-05 23:43:52 -0500

    Starting to make a home screen.  CSS is hard though, so it's pretty much nothing.

--- rolodx/main/views.py
+++ rolodx/main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
-	return HttpResponse('main page woo')
+	return render_to_response('home.html')
