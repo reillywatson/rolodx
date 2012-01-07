479273ffe1d8397928cccb6dcc0db3e8397eaf45
Joshua Sumali <j.sumali@gmail.com>
2012-01-07 12:23:03 -0500

    Adding a few comments regarding users and pros

--- rolodx/main/models.py
+++ rolodx/main/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # to consider: should Users just be Pros?  Is Pro even a good name?
+#	JS: I had to read around to figure out what 'Pro' meant. Maybe 'Professional' instead? 
+#	WRT Professionals and Users, perhaps a Professional can inherit from a User?
+#	This would make sense if we'll be having users who have their own business(es)
+#	listed on rolodx.
+
 # TODO: we probably need some sort of denormalization of ratings, so getting the average rating of a Pro isn't so slow
 
 
