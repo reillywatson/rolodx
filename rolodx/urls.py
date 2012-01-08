a2edfb2054f82419dcbd19e0cdd576bfefb367e2
Joshua Sumali <j.sumali@gmail.com>
2012-01-07 21:17:27 -0500

    Add a one column base template, link to it on the homepage

--- rolodx/urls.py
+++ rolodx/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'main.views.home'),
+    url(r'^onecolumnbase$', 'main.views.base'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
