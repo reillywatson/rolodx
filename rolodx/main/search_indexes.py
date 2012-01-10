e55ebec483b0416279e1c98611f0c6b26d280387
Reilly Watson <reillywatson@gmail.com>
2012-01-09 23:05:01 -0500

    Use templating thing better

--- rolodx/main/search_indexes.py
+++ rolodx/main/search_indexes.py
@@ -4,10 +4,5 @@ from main.models import Professional, Category, Service
 
 class ProfessionalIndex(SearchIndex):
 	text = CharField(document=True, use_template=True)
-	name = CharField(model_attr='name')
-
-	def index_queryset(self):
-		"""Used when the entire index for model is updated."""
-		return Professional.objects.all()
 
 site.register(Professional, ProfessionalIndex)
