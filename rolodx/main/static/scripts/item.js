52fbe6bee3b8724437c363264094549597c24c14
Robert Beghian <vasken@gmail.com>
2012-01-15 14:46:54 -0500

    Add javascript class for populating the item page metadata

--- rolodx/main/static/scripts/item.js
+++ rolodx/main/static/scripts/item.js
@@ -1,26 +1,93 @@
-Review = function (  review  ) {
-	this.init(review)
+Item = function ( data ) {
+	this.init(data);
+};
+
+Item.prototype = {
+	
+	name : null,
+	
+	job : null,
+	
+	rating : null,
+	
+	numRatings : null,
+	
+	address : null,
+	
+	website : null,
+	
+	email : null,
+	
+	hours : null,
+	
+	description : null,
+	
+	icon: null,
+	
+	constructor : Item,
+	
+	init : function( data ) {		
+		function starsNumToString(num) {
+			var rounded = Math.round(num* 2) / 2;
+			return (rounded == 0 ) ? "0_5" : rounded.toString().replace(/\./,'_');
+		}
+		
+		this.rating = starsNumToString(data.rating);
+		this.numRatings = data.numRatings;
+		this.name = data.name;
+		this.job = data.job;
+		this.address = data.address;
+		this.website = data.website;
+		this.email = data.email;
+		this.hours = data.hours;
+		this.description = data.description;
+		this.icon = data.icon;
+	},
+	
+	render : function () {
+		document.getElementById("item_metadata_icon");
+		document.getElementById("item_metadata_name").innerHTML = this.name;
+		document.getElementById("item_metadata_job").innerHTML = this.job;
+		document.getElementById("item_metadata_rating_stars").className = "rating_stars_" + this.rating;
+		document.getElementById("item_metadata_num_ratings").innerHTML = "(" + this.numRatings + ")";
+		document.getElementById("item_metadata_address").innerHTML = this.address;
+		document.getElementById("item_metadata_web").innerHTML = "Web: " + this.website;
+		document.getElementById("item_metadata_contact").innerHTML = "Contact: " + this.email;
+		document.getElementById("item_metadata_hours").innerHTML = "Hours: " + this.hours;
+		document.getElementById("item_metadata_description").innerHTML = this.description;
+	},
+	
+	toString : function() {
+		return this.name;
+	}
+}
+
+/**********************/
+/**		REVIEW		**/
+
+Review = function (  data  ) {
+	this.init(data)
 };
 
 Review.prototype = {
 	
 	author : null,
 	
-	rating : 0,
+	rating : null,
 	
 	text : null,
     
     constructor : Review,
 	
-	init : function( review ) {
+	init : function( data ) {
 		function starsNumToString(num) {
 			var rounded = Math.round(num* 2) / 2;
 			return (rounded == 0 ) ? "0_5" : rounded.toString().replace(/\./,'_');
 		}
 		
-		this.rating = starsNumToString(review.rating);
-		this.author = review.author;
-		this.text = review.text;
+		this.rating = starsNumToString(data.rating);
+		this.author = data.author;
+		this.text = data.text;
 		
 		
 	},
