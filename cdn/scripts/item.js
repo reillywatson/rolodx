ca30c02aed43645ebd6bb6ca5716749527970218
Robert Beghian <vasken@gmail.com>
2012-01-16 22:20:20 -0500

    Add cdn project that deploys to google app engine and fix resources (images/css) so they can be CDN'd better

---
+++ cdn/scripts/item.js
@@ -0,0 +1,115 @@
+Item = function ( data ) {
+	this.init(data);
+};
+
+Item.prototype = {
+	
+	name : null,
+	
+	occupation : null,
+	
+	rating : null,
+	
+	numRatings : null,
+	
+	street_address : null,
+	
+	website : null,
+	
+	email : null,
+	
+	hours : null,
+	
+	description : null,
+	
+	icon: null,
+	
+	constructor : Item,
+	
+	init : function( data ) {		
+		function starsNumToString(num) {
+			var rounded = Math.round(num* 2) / 2;
+			return (rounded == 0 ) ? "0_5" : rounded.toString().replace(/\./,'_');
+		}
+		
+		this.rating = starsNumToString(data.rating);
+		this.numRatings = data.numRatings;
+		this.name = data.name;
+		this.occupation = data.occupation;
+		this.street_address = data.street_address;
+		this.website = data.website;
+		this.email = data.email;
+		this.hours = data.hours;
+		this.description = data.description;
+		this.icon = data.icon;
+	},
+	
+	render : function () {
+		document.getElementById("item_metadata_icon");
+		document.getElementById("item_metadata_name").innerHTML = this.name;
+		document.getElementById("item_metadata_job").innerHTML = this.occupation;
+		document.getElementById("item_metadata_rating_stars").className = "rating_stars_" + this.rating;
+		document.getElementById("item_metadata_num_ratings").innerHTML = "(" + this.numRatings + ")";
+		document.getElementById("item_metadata_address").innerHTML = this.street_address;
+		document.getElementById("item_metadata_web").innerHTML = "Web: " + this.website;
+		document.getElementById("item_metadata_contact").innerHTML = "Contact: " + this.email;
+		document.getElementById("item_metadata_hours").innerHTML = "Hours: " + this.hours;
+		document.getElementById("item_metadata_description").innerHTML = this.description;
+	},
+	
+	toString : function() {
+		return this.name;
+	}
+}
+
+/**********************/
+/**		REVIEW		**/
+
+Review = function (  data  ) {
+	this.init(data)
+};
+
+Review.prototype = {
+	
+	author : null,
+	
+	rating : null,
+	
+	text : null,
+    
+    constructor : Review,
+	
+	content : "",
+	
+	init : function( data ) {
+		function starsNumToString(num) {
+			var rounded = Math.round(num* 2) / 2;
+			return (rounded == 0 ) ? "0_5" : rounded.toString().replace(/\./,'_');
+		}
+		this.rating = starsNumToString(data.rating);
+		this.author = data.author;
+		this.text = data.text;
+		
+        var template = "";
+		template +=	'<div class="item_review">';
+		template +=		'<div class="rating_stars_{stars}"></div>';
+		template +=		'<div class="item_review_text">{text}</div>';
+		template +=		'<div class="item_review_author">- {author}</div>';
+		template +=	'</div>';
+		
+		template = template.replace(/{stars}/,this.rating);
+		template = template.replace(/{text}/,this.text);
+		template = template.replace(/{author}/,this.author);
+		
+		this.content = template;
+	},
+	
+	render : function( parent ) {
+		parent.innerHTML += this.content
+	},
+	
+	toString : function() {
+		return "author: " +this.author + " rating " + this.rating + " text " + this.text; 
+	}
+	
+};
---
+++ cdn/scripts/search.js
@@ -0,0 +1,64 @@
+SearchResult = function( resultData ) {
+	this.init(resultData);
+}
+
+SearchResult.prototype = {
+	
+	id : null,
+	
+	icon : null,
+	
+	name : null, 
+	
+	occupation : null,
+	
+	rating : null,
+	
+	description : null,
+	
+	constructor : SearchResult,
+	
+	content : "",
+	
+	init : function( resultData ) {
+		function starsNumToString(num) {
+			var rounded = Math.round(num* 2) / 2;
+			return (rounded == 0 ) ? "0_5" : rounded.toString().replace(/\./,'_');
+		}
+		this.rating = starsNumToString(resultData.rating);
+		this.id = resultData.id;
+		this.icon = resultData.icon;
+		this.name = resultData.name;
+		this.occupation = resultData.occupation;
+		this.description = resultData.description;
+		
+		var template = "";
+		template +="<div class='pro_container'>";
+		template += "	<a class='no-link' href='/pro/{id}'>";
+		template +="		<img class='pro_icon'></img>";
+		template +="		<div class='pro_text'>{name}</div>";
+		template +="		<div class='pro_text'>{occupation}</div>";
+		template +="		<div class='pro_text'>"
+		template +="			<div class='pro_rating'>Rating:</div>"
+		template +="			<div class='rating_stars_{stars}'></div>"
+		template +="		</div>"
+		template +="		<div class='pro_description'>{description}</div>"
+		template +="		<div class='pro_separator'></div>"
+		template +="	</a>"
+		template +="</div>"
+		
+		template = template.replace(/{id}/,this.id);
+		template = template.replace(/{name}/,this.name);
+		template = template.replace(/{occupation}/,this.occupation);
+		template = template.replace(/{description}/, (this.description ? this.description : ""));
+		template = template.replace(/{stars}/,this.rating);
+		
+		this.content = template;
+	},
+	
+	render : function ( parent ) {
+		parent.innerHTML += this.content;
+	}
+
+}
+	
