a04246c5ce817fa181b7ed03725c28cb15cf2db0
Reilly Watson <reillywatson@gmail.com>
2012-01-21 11:25:17 -0500

    Be more robust

--- parsers/pageretriever.py
+++ parsers/pageretriever.py
@@ -1,5 +1,6 @@
 import BeautifulSoup
 import requests
+import time
 
 class PageRetriever:
 	baseUrl = ''
@@ -15,10 +16,13 @@ class PageRetriever:
 		tries = 0
 		while tries < 3:
 			arguments = dict(kwargs.items() + self.args.items())
-			response = requests.get(url, **arguments)
-			if response.status_code == 200:
-				return response.content
-			print 'failed with error %d, retrying!' % response.status_code
+			try:
+				response = requests.get(url, **arguments)
+				if response.status_code == 200:
+					return response.content
+				print 'failed with error %d, retrying!' % response.status_code
+			except:
+				pass
 			time.sleep(1)
 			tries = tries + 1
 		return None
