ec496fd9bf63edf750c9a1f9f8702a99b8d1634e
Reilly Watson <reillywatson@gmail.com>
2012-01-21 22:24:30 -0500

    Use geopy instead of rewriting it myself, because it does a better job

--- tools/geocode.py
+++ tools/geocode.py
@@ -1,27 +1,18 @@
-def geocode(address):
-	import requests
-	import json
-	import urllib
-	r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % urllib.quote(address))
-	response = json.loads(r.content)
-	results = response['results']
-	if len(results) == 0:
-		print 'no results for address %s' % address
-		return None
-	coords = response['results'][0]['geometry']['location']
-	# updating the models with these might be a good idea
-	# sweetaddress = response['results'][0]['formatted_address']
-	return (coords['lat'],coords['lng'])
-
 def populate_professionals_without_geocodes():
+	from geopy import geocoders
 	from rolodx.main.models import Professional
+	import sys, traceback
 	import decimal
 	pros = Professional.objects.filter(address_latitude=None)
+	g = geocoders.Google(domain='maps.google.ca')
 	for pro in pros:
 		if pro.street_address != None:
-			code = geocode(pro.street_address)
-			if code != None:
-				pro.address_latitude = decimal.Decimal(repr(code[0]))
-				pro.address_longitude = decimal.Decimal(repr(code[1]))
-				print code
-				pro.save()
+			try:
+				addr, (lat, lng) = g.geocode(pro.street_address)
+			except:
+				traceback.print_exc(file=sys.stdout)
+				continue
+			pro.address_latitude = decimal.Decimal(repr(lat))
+			pro.address_longitude = decimal.Decimal(repr(lng))
+			print lat,lng
+			pro.save()
