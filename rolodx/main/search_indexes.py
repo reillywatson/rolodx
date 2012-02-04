b1916647fda1c0cbcbcb29b5b6818340949b39ce
Joshua Sumali <j.sumali@gmail.com>
2012-02-04 14:57:03 -0600

    search indexing: when rebuilding indexes, include categories and all their parents (recursively)

--- rolodx/main/search_indexes.py
+++ rolodx/main/search_indexes.py
@@ -3,6 +3,13 @@ from haystack import site
 from main.models import Professional, Category, Service
 import decimal
 
+# Adds category's name and all parents names to a list of category name strings
+def PopulateCategoryListRecursive(category, categoryList):
+	if category.name not in categoryList:
+		categoryList.append(category.name)
+	if category.parent is not None:
+		PopulateCategoryListRecursive(category.parent, categoryList)
+
 # NOTE: Haystack doesn't currently recognize the tdouble field in the schemas
 # it generates, and we're using it for lat/long data.
 # So if you're modifying this, you may need to edit schema.xml by hand.
@@ -15,9 +22,12 @@ class ProfessionalIndex(SearchIndex):
 		self.prepared_data = super(ProfessionalIndex, self).prepare(object)
 
 		# Append each category name to field 'text'
+		categoryList = []
 		for category in object.categories.all():
-			if category is not None and category.name is not None:
-				self.prepared_data['text'] += ' %s' % category.name
+			PopulateCategoryListRecursive(category, categoryList)
+
+		for cat in categoryList:
+			self.prepared_data['text'] += ' %s' % cat
 
 		return self.prepared_data
 
