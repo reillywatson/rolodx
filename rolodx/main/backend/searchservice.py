8600f60db582c217e1661df3ec2bd7c1c22df9a4
Joshua Sumali <j.sumali@gmail.com>
2012-02-04 13:31:02 -0600

    Fix few bugs: 1. Fix paging - need to start at 0 index instead of 1 2. Only query solr once, instead of two times (once to get results, second to get total results) 3. Fix location based filtering

--- rolodx/main/backend/searchservice.py
+++ rolodx/main/backend/searchservice.py
@@ -4,21 +4,26 @@ from ui_models import SearchPageModel
 
 class SearchService:
 	
-	def search(self, text, lat, lng, radius, currentPage, itemsPerPage):
-
-		# By specifying the exact range of items we want with start and end, we only
-		# pull what we need from Solr, instead of getting all items.
-		start = currentPage*itemsPerPage
-		end = start+itemsPerPage;
-		searchResults = SearchQuerySet().auto_query(text)[start:end]
-		totalResults = SearchQuerySet().auto_query(text).count()
+	def ApplyLocationFiltering(self, lat, lng, radius, searchResultsQuery):
 		if -90 <= lat <= 90 and -180 <= lng <= 180:
 			minlat = lat - radius
 			minlng = lng - radius
 			maxlat = lat + radius
 			maxlng = lng + radius
-			print 'range: ', [minlat,minlng],[maxlat,maxlng]
-			searchResults.filter(address_latitude__range=[minlat,maxlat], address_longitude__range=sorted([maxlng,minlng]))
+			print 'range: ', [minlat, minlng], [maxlat, maxlng]
+			searchResultsQuery.filter(address_latitude__range=[minlat, maxlat], address_longitude__range=sorted([maxlng, minlng]))
+
+	def search(self, text, lat, lng, radius, currentPage, itemsPerPage):
+
+		# By specifying the exact range of items we want with start and end, we only
+		# pull what we need from Solr, instead of getting all items.
+		start = (currentPage-1)*itemsPerPage
+		end = start+itemsPerPage
+		searchResultsQuery = SearchQuerySet().auto_query(text)
+		self.ApplyLocationFiltering(lat, lng, radius, searchResultsQuery)
+
+		searchResults = searchResultsQuery[start:end]
+		totalResults = searchResultsQuery.count()
 		print [(a.address_latitude, a.address_longitude) for a in searchResults[:itemsPerPage]]
 		print [(a.object.address_latitude, a.object.address_longitude) for a in searchResults[:itemsPerPage]]
 		# HACK: iterate over the search results in order for pagination to
