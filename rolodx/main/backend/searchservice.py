58447e5de75b02e2208cb40c918156ee57377b34
Robert Beghian <vasken@gmail.com>
2012-02-12 21:30:29 -0500

    Ordering by rating by default, for search and categories

--- rolodx/main/backend/searchservice.py
+++ rolodx/main/backend/searchservice.py
@@ -1,24 +1,39 @@
 import haystack
 from haystack.query import SearchQuerySet
 from haystack.utils.geo import Point
-from ui_models import SearchPageModel
+
+class Order:
+	DISTANCE = 1
+	RATING = 2
 
 class SearchService:
-	def ApplyLocationFiltering(self, lat, lng, radius, searchResultsQuery):
+	
+	def ApplyLocationFiltering(self, lat, lng, radius, searchResultsQuery, ordering):
 		if -90 <= lat <= 90 and -180 <= lng <= 180:
 			topLeft = Point(lng-radius,lat-radius)
 			bottomRight = Point(lng+radius,lat+radius)
-			return searchResultsQuery.within('location', topLeft, bottomRight).distance('location', Point(lng,lat)).order_by('distance')
+			
+		if ordering == Order.RATING:
+			sqs = searchResultsQuery.within('location', topLeft, bottomRight).distance('location', Point(lng,lat)).order_by('-averageRating')
+		else:
+			sqs = searchResultsQuery.within('location', topLeft, bottomRight).distance('location', Point(lng,lat)).order_by('distance')
+		
+		return sqs
 
-	def search(self, text, lat, lng, radius, currentPage, itemsPerPage):
+	def search(self, text, lat, lng, radius, currentPage, itemsPerPage, ordering=Order.RATING):
 		# By specifying the exact range of items we want with start and end, we only
 		# pull what we need from Solr, instead of getting all items.
 		start = (currentPage-1)*itemsPerPage
 		end = start+itemsPerPage
-		searchResultsQuery = SearchQuerySet().auto_query(text)
-		if not lat is None and not lng is None:
-			searchResultsQuery = self.ApplyLocationFiltering(lat, lng, radius, searchResultsQuery)
 
+		# Filter By Location
+		if not lat is None and not lng is None:
+			searchResultsQuery = SearchQuerySet().auto_query(text) 
+			searchResultsQuery = self.ApplyLocationFiltering(lat, lng, radius, searchResultsQuery, ordering)
+		else:	
+			searchResultsQuery = SearchQuerySet().auto_query(text).order_by('-averageRating')
+		
+		# Paginate Results
 		searchResults = searchResultsQuery[start:end]
 		totalResults = searchResultsQuery.count()
 
