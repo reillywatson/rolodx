50404e665127182abc4de46567c3e6c633197a34
Reilly Watson <reillywatson@gmail.com>
2012-02-12 22:31:03 -0500

    Cleanup for search service

--- rolodx/main/backend/searchservice.py
+++ rolodx/main/backend/searchservice.py
@@ -7,19 +7,6 @@ class Order:
 	RATING = 2
 
 class SearchService:
-	
-	def ApplyLocationFiltering(self, lat, lng, radius, searchResultsQuery, ordering):
-		query = searchResultsQuery
-		if -90 <= lat <= 90 and -180 <= lng <= 180:
-			topLeft = Point(lng-radius,lat-radius)
-			bottomRight = Point(lng+radius,lat+radius)
-			query = searchResultsQuery.within('location', topLeft, bottomRight).distance('location', Point(lng,lat))
-		if ordering == Order.RATING:
-			query = query.order_by('-averageRating')
-		else:
-			query = query.within('location', topLeft, bottomRight).distance('location', Point(lng,lat)).order_by('distance')
-		return query
-
 	def search(self, text, lat, lng, radius, currentPage, itemsPerPage, ordering=Order.RATING):
 		# By specifying the exact range of items we want with start and end, we only
 		# pull what we need from Solr, instead of getting all items.
@@ -27,15 +14,19 @@ class SearchService:
 		end = start+itemsPerPage
 
 		# Filter By Location
-		if not lat is None and not lng is None:
-			searchResultsQuery = SearchQuerySet().auto_query(text) 
-			searchResultsQuery = self.ApplyLocationFiltering(lat, lng, radius, searchResultsQuery, ordering)
-		else:	
-			searchResultsQuery = SearchQuerySet().auto_query(text).order_by('-averageRating')
+		query = SearchQuerySet().auto_query(text)
+		if -90 <= lat <= 90 and -180 <= lng <= 180:
+			topLeft = Point(lng-radius,lat-radius)
+			bottomRight = Point(lng+radius,lat+radius)
+			query = query.within('location', topLeft, bottomRight).distance('location', Point(lng,lat))
+		if ordering == Order.RATING:
+			query = query.order_by('-averageRating')
+		else:
+			query = query.within('location', topLeft, bottomRight).distance('location', Point(lng,lat)).order_by('distance')
 		
 		# Paginate Results
-		searchResults = searchResultsQuery[start:end]
-		totalResults = searchResultsQuery.count()
+		searchResults = query[start:end]
+		totalResults = query.count()
 
 		searchObjects = [a.object for a in searchResults]
 		return SearchResult(searchObjects, itemsPerPage, currentPage, totalResults, text)
