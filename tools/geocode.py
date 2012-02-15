b1aafb6ce8fc5652dc73393d3bef319550061966
Joshua Sumali <j.sumali@gmail.com>
2012-02-15 17:34:23 -0500

    In this commit: -change parsers/healthmedia.py to re-use categories instead of creating new ones each time -update db.sqlite with the new categories, deleting the old duplicated ones (had to re-import all doctors) -add a 0.5 second timeout to geocode.py, since google throttles us otherwise

--- tools/geocode.py
+++ tools/geocode.py
@@ -3,18 +3,25 @@ def populate_professionals_without_geocodes():
 	from rolodx.main.models import Professional
 	import sys, traceback
 	import decimal
+	import time
 	pros = Professional.objects.filter(address_latitude=None)[100:]
 	g = geocoders.Google()
+	print 'Populating geocodes for %s professionals' % len(pros)
 	for pro in pros:
 		if pro.street_address != None:
 			try:
 				results = g.geocode(pro.street_address, exactly_one=False)
 			except:
 				traceback.print_exc(file=sys.stdout)
-				print 'failure address: %s' % pro.street_address
+				try:
+					print 'failure address: %s' % pro.street_address
+				except:
+					print 'could not print address for pro.id %s' % pro.id
+				time.sleep(0.5)
 				continue
 			pro.address_latitude = decimal.Decimal(repr(results[0][1][0]))
 			pro.address_longitude = decimal.Decimal(repr(results[0][1][1]))
 			print pro.street_address
 			print pro.address_latitude, pro.address_longitude
 			pro.save()
+			time.sleep(0.5)
