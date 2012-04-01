c9fca2409fd71c94d2b1e4034ffc56c7ee12cee9
Robert Beghian <vasken@gmail.com>
2012-03-31 23:21:38 -0400

    Do some work to edit pro info

--- rolodx/main/static/scripts/proAdder.js
+++ rolodx/main/static/scripts/proAdder.js
@@ -25,12 +25,30 @@ ProfessionalAdder.prototype = {
 		
 		template  = "<div id='pro_adder_large_icon'></div>";
 		template += "<div id='pro_adder_large_container'>";
-		template += "	<div id='pro_adder_large_name' class='pro_adder_large_txt'>Name</div>";
-		template += "	<div id='pro_adder_large_category' class='pro_adder_large_txt'>Category</div>";
-		template += "	<div id='pro_adder_large_address' class='pro_adder_large_txt'>Address</div>";
-		template += "	<div id='pro_adder_large_phone' class='pro_adder_large_txt'>Phone Number</div>";
-		template += "	<div id='pro_adder_large_rating' class='pro_adder_large_txt'>Rating</div>";
-		template += "	<div id='pro_adder_large_review' class='pro_adder_large_txt'>Review</div>";
+		template += "	<div id='pro_adder_large_name' class='pro_adder_edit_text'>Name</div>";
+		template += "	<div id='pro_adder_large_name_edit' class='gone'>"
+		template += "		<input id='pro_adder_large_name_text' type='text' value='Name'></input>"
+		template += "	</div>";
+		template += "	<div id='pro_adder_large_category' class='pro_adder_edit_text'>Category</div>";
+		template += "	<div id='pro_adder_large_category_edit' class='gone'>"
+		template += "		<input id='pro_adder_large_category_text' type='text' value='Name'></input>"
+		template += "	</div>";
+		template += "	<div id='pro_adder_large_address' class='pro_adder_edit_text'>Address</div>";
+		template += "	<div id='pro_adder_large_address_edit' class='gone'>"
+		template += "		<input id='pro_adder_large_address_text' type='text' value='Name'></input>"
+		template += "	</div>";
+		template += "	<div id='pro_adder_large_phone' class='pro_adder_edit_text'>Phone Number</div>";
+		template += "	<div id='pro_adder_large_phone_edit' class='gone'>"
+		template += "		<input id='pro_adder_large_phone_text' type='text' value='Name'></input>"
+		template += "	</div>";
+		template += "	<div id='pro_adder_large_rating' class='pro_adder_edit_text'>Rating</div>";
+		template += "	<div id='pro_adder_large_rating_edit' class='gone'>"
+		template += "		<input id='pro_adder_large_rating_text' type='text' value='Name'></input>"
+		template += "	</div>";
+		template += "	<div id='pro_adder_large_review' class='pro_adder_edit_text'>Review</div>";
+		template += "	<div id='pro_adder_large_review_edit' class='gone'>"
+		template += "		<input id='pro_adder_large_review_text' type='text' value='Name'></input>"
+		template += "	</div>";
 		template += "</div>";
 		this.largeElement = document.createElement("div");
 		this.largeElement.className = "pro_adder_large gone";
@@ -50,6 +68,32 @@ ProfessionalAdder.prototype = {
 	
 	render : function( parent ) {
 		parent.appendChild(this.element);
-	}
+		document.getElementById("pro_adder_large_name").onclick = this.editText;
+		document.getElementById("pro_adder_large_name_text").onmouseout = this.onBlur;
+		document.getElementById("pro_adder_large_category").onclick = this.editText;
+		document.getElementById("pro_adder_large_category_text").onmouseout = this.onBlur;
+		document.getElementById("pro_adder_large_address").onclick = this.editText;
+		document.getElementById("pro_adder_large_address_text").onmouseout = this.onBlur;
+		document.getElementById("pro_adder_large_phone").onclick = this.editText;
+		document.getElementById("pro_adder_large_phone_text").onmouseout = this.onBlur;
+		document.getElementById("pro_adder_large_rating").onclick = this.editText;
+		document.getElementById("pro_adder_large_rating_text").onmouseout = this.onBlur;
+		document.getElementById("pro_adder_large_review").onclick = this.editText;
+		document.getElementById("pro_adder_large_review_text").onmouseout = this.onBlur	;
+	},
 
+	editText : function() {
+		this.style.display = "none";
+		document.getElementById(this.id+"_edit").className = "";
+	},
+	
+	onBlur : function() {
+		var display = document.getElementById(this.id.replace("_text", ""));
+		var edit = document.getElementById(this.id.replace("_text", "_edit"));
+		
+		display.textContent = this.value;
+		display.style.display = "block";
+		display.className = "pro_adder_text";
+		edit.className = "gone";
+	},
 };
